6e9f76812a1c9f49bcd1e1c3fbc932d72e563e0e
vidur2 <vmod2005@gmail.com>
2021-06-06 16:45:10 -0400

    Added new variable, 'Ranked PPG', to the dataframe

--- Basketball Reference Usage.py
+++ Basketball Reference Usage.py
@@ -16,20 +16,39 @@ def main():
         season = BasketballReferencePull('https://www.basketball-reference.com/leagues/NBA_' + true_year + '_per_game.html', true_year)
         season.rename(columns = {"Player" : "Player" + true_year})
         season_index.append(season)
+    
     seasons = pd.concat(season_index, axis = 0, ignore_index = True, join = 'inner')
-    seasons.sort_values('Player', inplace=True)
+    seasons.sort_values('Player', axis=0, inplace=True, ascending=True, ignore_index=True)
     playerNames = set(seasons['Player'].copy())
     groupedSeasons = seasons.groupby('Player')
-    sortedDf = groupedSeasons.get_group('Paul Pierce*')
-    sortedDf.sort_values("Player's age on February 1 of the season",inplace=True)
-    print(list(sortedDf["Player's age on February 1 of the season"].copy()))
-    pointsPerGame = list(sortedDf['Points Per Game'].copy())
-    maxPpg = max(int(pointsPerGame))
+    playerNames = list(playerNames)
     normalizedPPG = []
-    for year in pointsPerGame:
-        normalizedPPG.append((int(year)/maxPpg) * 100)
-    sortedDf['NormalizedPPG'] = normalizedPPG
-    print(sortedDf)
+    playerNames.sort()
+    for player in playerNames:
+        sortedDf = groupedSeasons.get_group(player)
+        
+        pointsPerGame = list(sortedDf['Points Per Game'].copy())
+        floatPointsPerGame = []
+
+        for year in pointsPerGame:
+            floatPointsPerGame.append(float(year))
+        maxPpg = max(floatPointsPerGame)
+        if maxPpg == 0:
+            for _ in floatPointsPerGame:
+                normalizedPPG.append(0)
+        else:
+            for year in floatPointsPerGame:
+                normalizedPPG.append((year/maxPpg) * 100)
+    seasons['RankedPPG'] = normalizedPPG
+    statistics = list(seasons.columns)
+    statistics.remove('Rank')
+    statistics.remove('Player')
+    statistics.remove('Position')
+    statistics.remove('Team')
+    for stat in statistics:
+        pd.to_numeric(seasons[stat], downcast="float")
+    groupedSeasons = seasons.groupby('Player')
+    print(groupedSeasons.get_group('Paul George'))
 
 if __name__ == '__main__':
     main()
