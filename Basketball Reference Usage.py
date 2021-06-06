3855ffa8d72025a50878023f388cf7e3b2adc4ee
vidur2 <vmod2005@gmail.com>
2021-06-06 12:02:35 -0400

    Commit on Basketball Reference Usage.py

--- Basketball Reference Usage.py
+++ Basketball Reference Usage.py
@@ -7,6 +7,7 @@ Use newfound pandas knowledge to yeet project into existence
 # Imports
 from basketballRefCommands import *
 
+
 def main():
     season_index = []
     for i in range(30):
@@ -16,8 +17,19 @@ def main():
         season.rename(columns = {"Player" : "Player" + true_year})
         season_index.append(season)
     seasons = pd.concat(season_index, axis = 0, ignore_index = True, join = 'inner')
-    seasons.sort_values("Player", ascending=True)
-    print(seasons)
+    seasons.sort_values('Player', inplace=True)
+    playerNames = set(seasons['Player'].copy())
+    groupedSeasons = seasons.groupby('Player')
+    sortedDf = groupedSeasons.get_group('Paul Pierce*')
+    sortedDf.sort_values("Player's age on February 1 of the season",inplace=True)
+    print(list(sortedDf["Player's age on February 1 of the season"].copy()))
+    pointsPerGame = list(sortedDf['Points Per Game'].copy())
+    maxPpg = max(int(pointsPerGame))
+    normalizedPPG = []
+    for year in pointsPerGame:
+        normalizedPPG.append((int(year)/maxPpg) * 100)
+    sortedDf['NormalizedPPG'] = normalizedPPG
+    print(sortedDf)
 
 if __name__ == '__main__':
     main()
